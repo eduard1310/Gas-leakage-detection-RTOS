f21e11f184ca67c16e44696d3bc6d58f84aa631f
eduard1310 <56159022+eduard1310@users.noreply.github.com>
2021-01-13 13:42:39 +0200

    Update trans_sens.ino

--- trans_sens/trans_sens.ino
+++ trans_sens/trans_sens.ino
@@ -1,27 +1,33 @@
 #include<Arduino_FreeRTOS.h>
 #include<Wire.h>
 #include "task.h"
+
+//gas sensor pin
 #define SensePin A0
 
 #define INCLUDE_vTaskPrioritySet 1
 
-
+//set priorities for the acquisition and transmission task
 #define ACQUISITION_TASK_PRIORITY  ( tskIDLE_PRIORITY +1 )
 #define TRANSMISSION_TASK_PRIORITY ( tskIDLE_PRIORITY )
 
+//global variables
 unsigned int x =0;
 unsigned int transmissionVal = 0;
 
+//declare task_handle
 TaskHandle_t TransmissionTask_Handle;
 TaskHandle_t AcquisitionTask_Handle;
 
+
+//acquisition task
 void AcquisitionTask(void *pvParameters)
 {
   TickType_t xLastWakeTime = xTaskGetTickCount();
-  /*x = analogRead(SensePin);
-  transmissionVal = map(x , 0 , 1023 , 0 , 100);*/
+  
   while(1)
   {
+  //read the value from pin A0
   x = analogRead(SensePin);
   transmissionVal = map(x , 0 , 1023 , 0 , 100);
 
@@ -33,15 +39,17 @@ void AcquisitionTask(void *pvParameters)
   
 }  
 
+
+//transmission_task (using i2c)
 void TransmissionTask(void *pvParameters)
 {
   TickType_t xLastWakeTime = xTaskGetTickCount();
   
   while(1)
   {
-    Wire.beginTransmission(9); // transmit to device #9
-  Wire.write(transmissionVal);              // sends x 
-  Wire.endTransmission();    // stop transmitting
+    Wire.beginTransmission(9);   // transmit to device #9
+    Wire.write(transmissionVal); // sends x 
+    Wire.endTransmission();      // stop transmitting
     xTaskDelayUntil(&xLastWakeTime , (1000 / portTICK_PERIOD_MS));
   }
 }
@@ -49,7 +57,8 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Wire.begin();
-  ////////////////////////////////////////////////////////////////////////
+  
+  // Create tasks
   xTaskCreate(AcquisitionTask , "Task citire senzor" , 255 , NULL , ACQUISITION_TASK_PRIORITY , &AcquisitionTask_Handle);
   xTaskCreate(TransmissionTask , "Task transmisie " ,255 , NULL , TRANSMISSION_TASK_PRIORITY , &TransmissionTask_Handle);
 }
